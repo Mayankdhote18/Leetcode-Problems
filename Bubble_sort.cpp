d56eb032263d5fef1788981129749af2f1a65b45
Mayankdhote18 <mayankdhote4316@gmail.com>
2025-07-30 19:43:57 +0530

    insertion sort and little changes in bubble sort

--- Bubble_sort.cpp
+++ Bubble_sort.cpp
@@ -1,33 +1,39 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void bubble_sort(int arr[],int n);
-for(int i=n-1;i>=0;i--)
+void bubble_sort(int arr[], int n)
 {
-    for(int j=0;j<i-1;j++)
+    for (int i = n - 1; i >= 0; i--)
     {
-        if(arr[j>arr[j+1]])
+        for (int j = 0; j < i; j++)
         {
-            int temp = arr[j];
-            arr[j]= arr[j+1];
-            arr[j+1]= temp;
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
     }
 }
 
-int main(){
- int n;
-cin>>n;
-int arr[n];
-for(int i=0 ;i<n;i++)
+int main()
 {
-    cin>>arr[i]
-}
-bubble_sort(arr, int n)
-for(int i=0;i<n;i++)
-{
-    cout<< arr[i]<<" ";
-}
- 
-return 0;
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    bubble_sort(arr, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
 }
