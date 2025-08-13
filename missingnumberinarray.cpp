94336276bbf90c3566102620fac70cdde7c4145d
Mayankdhote18 <mayankdhote4316@gmail.com>
2025-08-14 00:10:31 +0530

    missing number problem

---
+++ missingnumberinarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n =nums.size();
+        int sum=0;
+         int actualsum= (n)*(n+1)/2;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum += nums[i];
+        }
+       int x = (actualsum - sum);
+        return x;
+    }
+    
+};
